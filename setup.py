3f0aadc223491d1aaaf477f080d92e74efd8931c
Craig Vyvial <cp16net@gmail.com>
2017-02-16 21:35:58 -0600

    fix the requirements for PyYAML

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+import os
 from setuptools import setup
 from setuptools import find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='yaml2json',
@@ -9,6 +13,7 @@ setup(
     author_email='cp16net@gmail.com',
     license='Apache',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'yaml2json=yaml2json.yaml2json:main',
